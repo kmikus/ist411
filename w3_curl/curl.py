45974753016104e48375eff847d5ca09126f7322
kmikus <root@DESKTOP-9GRDGK7.localdomain>
2017-09-07 11:45:45 -0400

    w3 lab complete

--- w3_curl/curl.py
+++ w3_curl/curl.py
@@ -1,8 +1,30 @@
-import sys, urllib.parse, urllib.request
+# Project: urllib, json and python lab
+# Purpose Details: To read data from a url and output into json file
+# Course: IST411
+# Author: Kevin Mikus
+# Date Developed: 9/7/17
+# Last Date Changed: 9/7/17
+# Rev: 0.1
 
-url = "https://jsonplaceholder.typicode.com/posts/1/comments"
-clean_url = urllib.parse.urlparse(url)
+import sys, urllib.parse, urllib.request, json
+ 
+try:
+	# parse url for encoding
+	url = "https://jsonplaceholder.typicode.com/posts/1/comments"
+	clean_url = urllib.parse.urlparse(url)
 
-# print(clean_url)
+	# request body from url
+	response = urllib.request.urlopen(clean_url.geturl())
+	payload = response.read()
+	encoding = response.info().get_content_charset("utf-8")
 
+	# convert to json format
+	jsonStream = json.loads(payload.decode(encoding))
+	jsonDump = json.dumps(jsonStream, indent=4)
+	with open("curl.json", "w") as outFile:
+		outFile.write(jsonDump)
+	print(jsonDump)
+except:
+	e = sys.exc_info()[0]
+	print("error: {}".format(e))
 
