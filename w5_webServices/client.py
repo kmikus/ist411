1961e6b99e19964ef7d37360e10926a708cf25d5
kmikus <root@DESKTOP-9GRDGK7.localdomain>
2017-09-20 15:41:43 -0400

    week 5

--- w5_webServices/client.py
+++ w5_webServices/client.py
@@ -2,6 +2,14 @@ import socket
 try:
 	clientSocket = socket.socket()
 	clientSocket.connect((socket.gethostname(), 8080))
-	clientSocket.sendall(bytes("This is a message"))	
+	with open("input.json", "rb") as fh:
+		print("Sending...")
+		data = fh.read(1024)
+		while data:
+			print("Sending...")
+			clientSocket.send(data)
+			data = fh.read(1024)
+	print("Transmission complete")
+	clientSocket.shutdown(socket.SHUT_RDWR)
 except Exception as e:
 	print(e)
--- w5_webServices/server.py
+++ w5_webServices/server.py
@@ -6,9 +6,16 @@ try:
 	print("Bind the socket to public host, and a well-known port 8080")
 	serverSocket.listen(5)
 	print("Become a server socket")
-	while True:
-		print("Accepts connections from outside")
-		(clientSocket, address) = serverSocket.accept()
-		# PRINT CLIENT SIDE CODE HERE
+	with open("output.json", "wb") as fh:
+		while True:
+			print("Accepts connections from outside")
+			clientSocket, address = serverSocket.accept()
+			print("Received connection from {}".format(address))
+			data = clientSocket.recv(1024)
+			while data:
+				print("Receiving data...")
+				fh.write(data)
+				data = clientSocket.recv(1024)
+			print("Done receiving")
 except Exception as e:
 	print(e)
