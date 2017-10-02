a9cc408203bef6068a72d36c298a49948a1ac5cc
kmikus <root@DESKTOP-9GRDGK7.localdomain>
2017-10-02 12:16:28 -0400

    week 7 start

--- w6_rest/run.py
+++ w6_rest/run.py
@@ -1,10 +1,10 @@
-#Project: REST API using Eve framework
+# Project: REST API using Eve framework
 # Purpose Details: Set up a REST API using Eve and cURL contents to MongoDB
 # Course: IST411
 # Author: Kevin Mikus
 # Date Developed: 09/26/17
-# Last Date Changed: 09/26/17
-# Rev: 0.1
+# Last Date Changed: 09/28/17
+# Rev: 0.2
 
 from eve import Eve
 app = Eve()
--- w6_rest/settings.py
+++ w6_rest/settings.py
@@ -1,3 +1,11 @@
+# Project: REST API using Eve framework
+# Purpose Details: Set up a REST API using Eve and cURL contents to MongoDB
+# Course: IST411
+# Author: Kevin Mikus
+# Date Developed: 09/26/17
+# Last Date Changed: 09/28/17
+# Rev: 0.2
+
 MONGO_HOST = 'localhost'
 MONGO_PORT = 27017
 MONGO_DBNAME = 'apitest'
